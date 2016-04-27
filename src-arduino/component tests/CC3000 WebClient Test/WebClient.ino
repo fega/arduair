27e5d4e64fb4f707bb84947a8ab3efc2761e90b9
Fabian <fega.hg@gmail.com>
2016-04-27 13:17:51 -0500

    minor improvements

--- src-arduino/component tests/CC3000 WebClient Test/WebClient.ino
+++ src-arduino/component tests/CC3000 WebClient Test/WebClient.ino
@@ -41,8 +41,8 @@ It might not work on all networks!
 Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT,
                                          SPI_CLOCK_DIVIDER); // you can change this clock speed
 
-#define WLAN_SSID       "FABIAN"           // cannot be longer than 32 characters!
-#define WLAN_PASS       "63856199"
+#define WLAN_SSID       "myNetwork"           // cannot be longer than 32 characters!
+#define WLAN_PASS       "myPassword"
 // Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
 #define WLAN_SECURITY   WLAN_SEC_WPA2
 
@@ -51,8 +51,8 @@ Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ
                                    // you're accessing is quick to respond, you can reduce this value.
 
 // What page to grab!
-#define WEBSITE      "www.adafruit.com"
-#define WEBPAGE      "/testwifi/index.html"
+#define WEBSITE      "arduair.herokuapp.com"
+#define WEBPAGE      "/test/"
 
 
 /**************************************************************************/

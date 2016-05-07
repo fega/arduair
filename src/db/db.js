4de19a0aa5df9233f27e6f31949d61648f54a411
Fabian <fega.hg@gmail.com>
2016-05-07 12:29:49 -0500

    Arduino Coded Updated
    But it's  too big for the Arduino's memory

---
+++ src/db/db.js
@@ -0,0 +1,5 @@
+//URL DE LA BASE DE DATOS
+module.exports = {
+  	'url' : 'mongodb://admin:admin@ds019940.mlab.com:19940/arduair'
+  	//'url' : 'mongodb://localhost:27017',
+	}
--- src/gulpfile.js
+++ src/gulpfile.js
@@ -6,6 +6,7 @@ var csslint = require('gulp-csslint');
 var browserSync = require('browser-sync');
 var nodemon = require('gulp-nodemon');
 var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
 
 var BROWSER_SYNC_RELOAD_DELAY = 500;// we'd need a slight delay to reload browsers, connected to browser-sync after restarting nodemon
 
@@ -28,18 +29,26 @@ gulp.task('nodemon', function (cb) {
       }, BROWSER_SYNC_RELOAD_DELAY);
     });
 });
+/*///////////////////////////////////////
+JS linter
+///////////////////////////////////////*/
+gulp.task('jslint', function() {
+  return gulp.src('**/*.js','!./node_modules/*')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('csslint', function() {
+  gulp.src(['./public/css/style.css','./public/css/plataforma.css'])
+    .pipe(csslint())
+    .pipe(csslint.reporter());
+});
 
 gulp.task('browser-sync', ['nodemon'], function () {
-  // for more browser-sync config options: http://www.browsersync.io/docs/options/
   browserSync({
-    // informs browser-sync to proxy our expressjs app which would run at the following location
     proxy: 'http://localhost:3000',
-    // informs browser-sync to use the following port for the proxied app
-    // notice that the default port is 3000, which would clash with our expressjs
     port: 4000,
-    // open the proxied app in chrome
-    //browser: ['google-chrome']
-  });
+   });
 });
 
 gulp.task('js',  function () {
@@ -55,14 +64,14 @@ gulp.task('css', function () {
         .pipe(concat('css.min.css'))
         .pipe(gulp.dest('./public/css'))
         .pipe(browserSync.reload({ stream: true }));
-})
+});
 
 gulp.task('bs-reload', function () {
   browserSync.reload();
 });
 
 gulp.task('default', ['browser-sync'], function () {
-  gulp.watch('public/**/*.js',   ['js']);
+  gulp.watch('public/**/*.js',   ['js', 'jslint']);
   gulp.watch('public/**/*.css',  ['css']);
   gulp.watch('public/**/*.html', ['bs-reload']);
   gulp.watch('views/**/*.handlebars', ['bs-reload']);

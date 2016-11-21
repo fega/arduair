56e51ac32b8d8e0021518f6187ff24532d6c4418
fega <fega.hg@gmail.com>
2016-11-21 10:42:00 -0500

    refractored datecheck

--- src/lib/datecheck.js
+++ src/lib/datecheck.js
@@ -1,40 +1,37 @@
 module.exports=datecheck;
 /**
- * checkquea que los parametros ingresados al request sean una decha valida
+ * Check if the params day,
  * @param  {Object} req    the request
  * @param  {Object} res    the response
  * @param  {Function} next the callback function
  * @return {Function}      return an error or the next() callback
  */
 function datecheck(req,res,next){
-	//console.log('DAY: '+ req.params.day);
-	//console.log('MONTH: '+ req.params.month);
-	//console.log('YEAR: '+ req.params.year);
+	var day= req.params.day;//console.log('DAY: '+ day);
+	var month= req.params.month;//console.log('MONTH: '+ month);
+	var year= req.params.year;//console.log('YEAR: '+ year);
+	var hour= req.params.hour;
+	var minute= req.params.minute;
 
-	if(!req.params.day || req.params.day>31 || req.params.day<0 || isNaN(req.params.day)){
-		req.message+='Problemas el parametro dia, <br>';
-		console.log('req.message');
-	}
-	if(!req.params.month || req.params.month>12 || req.params.month<0 || isNaN(req.params.month)){
-		req.message+='Problemas el parametro mes, <br>';
-	}
-	if(!req.params.year || req.params.year<=2000 || isNaN(req.params.year)){
-		//esto esta fallando...
-		req.messag+='Problemas el parametro año, <br>';
-	}
-	if(!req.params.hour || req.params.hour>24 || req.params.hour<0 || isNaN(req.params.hour)){
-		req.message+='Problemas el parametro hora, <br>';
-	}
-	if(!req.params.minute || req.params.minute>60 || req.params.minute<0 || isNaN(req.params.minute)){
-		req.message+='Problemas el parametro minuto, <br>';
-	}
+	checkRange(day,  31,   0,'day');
+	checkRange(month,12,   0,'month');
+	checkRange(year, 10000,2000,'year');
+	checkRange(hour, 24,   0,'hour');
+	checkRange(minute,60,  0,'minute');
+
 	//console.log(req.message);
 	if (isEmpty(req.message)){
 		return next();
 	}else{
 		res.send(req.message);
 	}
-}
-function isEmpty(str) {
-    return (!str || 0 === str.length);
+	function checkRange(val,min,max,name){
+		if(!val || val>max || val<min || isNaN(val)){
+			req.message+=`Problems with ${name}, <br>`;
+			console.log('req.message');
+		}
+	}
+	function isEmpty(str) {
+		return (!str || 0 === str.length);
+	}
 }
